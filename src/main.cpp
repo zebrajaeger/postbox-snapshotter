c29861e324f6431dc161cf33268b8330197988dd
Lars Brandt <zebrajaeger@zebrajaeger.de>
2024-08-23 13:50:44 +0200

    [fix] no ntp connection

--- src/main.cpp
+++ src/main.cpp
@@ -87,6 +87,7 @@ void setup() {
     }
   } else {
     takePictureAndSendFTP();
+    WiFi.mode(WIFI_OFF);
     enterDeepSleep();
   }
 }
@@ -140,6 +141,7 @@ void takePictureAndSendFTP() {
     Serial.println("Camera Capture Failed");
   }
 
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
     Serial.println("Failed to obtain time");
@@ -170,41 +172,6 @@ void enterDeepSleep() {
   esp_sleep_enable_ext0_wakeup(GPIO_NUM_12, 0);  // WAKEUP_PIN als Wakeup Quelle
   rtc_gpio_isolate(GPIO_NUM_4);
   esp_deep_sleep_start();
-}
-
-void printLocalTime() {
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println("Failed to obtain time");
-    return;
-  }
-
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  Serial.print("Day of week: ");
-  Serial.println(&timeinfo, "%A");
-  Serial.print("Month: ");
-  Serial.println(&timeinfo, "%B");
-  Serial.print("Day of Month: ");
-  Serial.println(&timeinfo, "%d");
-  Serial.print("Year: ");
-  Serial.println(&timeinfo, "%Y");
-  Serial.print("Hour: ");
-  Serial.println(&timeinfo, "%H");
-  Serial.print("Hour (12 hour format): ");
-  Serial.println(&timeinfo, "%I");
-  Serial.print("Minute: ");
-  Serial.println(&timeinfo, "%M");
-  Serial.print("Second: ");
-  Serial.println(&timeinfo, "%S");
-
-  Serial.println("Time variables");
-  char timeHour[3];
-  strftime(timeHour, 3, "%H", &timeinfo);
-  Serial.println(timeHour);
-  char timeWeekDay[10];
-  strftime(timeWeekDay, 10, "%A", &timeinfo);
-  Serial.println(timeWeekDay);
-  Serial.println();
 }
 
 void loop() {}
